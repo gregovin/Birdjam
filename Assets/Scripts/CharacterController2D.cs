7d8abe8215257add0b0591d66c9e06259ee60578
gregovin <gregory.w.hess@gmail.com>
2020-05-25 20:12:31 -0700

    Made stealth work

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -5,15 +5,12 @@ using System;
 public class CharacterController2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
     public Transform m_This;
-
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
@@ -36,7 +33,6 @@ public class CharacterController2D : MonoBehaviour
 	public class BoolEvent : UnityEvent<bool> { }
 
 	public BoolEvent OnCrouchEvent;
-	private bool m_wasCrouching = false;
 
 	private void Awake()
 	{
@@ -46,7 +42,7 @@ public class CharacterController2D : MonoBehaviour
 
         if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
-
+        Flip();
 		if (OnCrouchEvent == null)
 			OnCrouchEvent = new BoolEvent();
 
@@ -79,14 +75,6 @@ public class CharacterController2D : MonoBehaviour
 
         }
 		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-			{
-				crouch = true;
-			}
-		}
 
 		//only control the player if grounded or airControl is turned on
 		if (m_Grounded || m_AirControl)
--- Assets/Scripts/StealthScript.cs
+++ Assets/Scripts/StealthScript.cs
@@ -5,34 +5,72 @@ using UnityEngine;
 public class StealthScript : MonoBehaviour
 {
     public Collider2D[] colliders;
+    public Collider2D attackBlob;
     float timeKeeper = 0;
     int colliderChoosen = 0;
+    int dir = 1;
+    bool facing = false;
+    public Transform defaultPos;
+    private Rigidbody2D m_Rigidbody;
+    private Transform m_Transform;
+    bool walking = false;
+    GameObject target;
     Animator controller;
     // Start is called before the first frame update
     void Start()
     {
         colliders[colliderChoosen].enabled = false;
         controller = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_Transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        controller.SetBool("Looking", !controller.GetBool("Looking"));
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("it works");
+        controller.SetBool("Looking", facing);
     }
     void FixedUpdate()
     {
-        timeKeeper += Time.fixedDeltaTime;
-        if (timeKeeper > 5)
+        if (!walking)
         {
-            colliders[colliderChoosen].enabled = true;
-            colliderChoosen = (colliderChoosen + 1) % colliders.Length;
-            colliders[colliderChoosen].enabled = false;
-            timeKeeper = 0;
+            timeKeeper += Time.fixedDeltaTime;
+            if (timeKeeper > 5)
+            {
+                colliders[colliderChoosen].enabled = true;
+                colliderChoosen = (colliderChoosen + 1) % colliders.Length;
+                colliders[colliderChoosen].enabled = false;
+                timeKeeper = 0;
+                facing = !facing;
+            }
+        } else
+        {
+            Vector3 relativeDisplacement = target.transform.position - m_Transform.position;
+            Vector3 normalDisplacement = relativeDisplacement;
+            normalDisplacement.Normalize();
+            normalDisplacement.y = 0;
+            if(normalDisplacement.x * dir < 0)
+            {
+                Vector3 theScale = transform.localScale;
+                theScale.x *= -1;
+                dir *= -1;
+                transform.localScale = theScale;
+            }
+            m_Rigidbody.velocity = normalDisplacement;
+            controller.SetBool("Attacking", relativeDisplacement.magnitude < 2);
+            attackBlob.enabled = relativeDisplacement.magnitude < 2;
+            Debug.Log(relativeDisplacement.magnitude);
+            if(relativeDisplacement.magnitude > 4)
+            {
+                walking = false;
+                controller.SetBool("Spotted", false);
+            }
         }
+    }
+    public void Seen(GameObject other)
+    {
+        walking = true;
+        controller.SetBool("Spotted", true);
+        target = other;
     }
 }
