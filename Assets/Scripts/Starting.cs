ad64c774fcd755b3a1caaad47308d07a5bfc0442
gregovin <gregory.w.hess@gmail.com>
2020-05-30 14:42:20 -0700

    Fixing some commit wierdness

---
+++ Assets/Scripts/Starting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Starting : MonoBehaviour
+{
+    public void LetItBegin()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
