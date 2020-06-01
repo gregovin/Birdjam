3caaf40382608aaff0dfccfef60384b61c52c0f6
gregovin <gregory.w.hess@gmail.com>
2020-05-31 20:31:41 -0700

    Added a points system and a nest

---
+++ Assets/Scripts/PointsDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsDetector : MonoBehaviour
+{
+    public LayerMask grabbable;
+    public Text score;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collided");
+        if (((1<<other.gameObject.layer) & grabbable) == 1 << other.gameObject.layer)
+        {
+            score.text = "" + (int.Parse(score.text) + 100);
+            Destroy(other.gameObject);
+        }
+    }
+}
